4ba95588a28dff09b33f9ec1862ce183cb846173
CCWebb14 <CCWebb14@gmail.com>
2023-02-06 14:53:36 -0800

    add, subtract, multiply, divide functions created

---
+++ app.js
@@ -0,0 +1,26 @@
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 + num2;
+
+const multiply = (num1, num2) => num1 + num2;
+
+const divide = (num1, num2) => num1 + num2;
+
+const operate = (num1, operator, num2) => {
+  if (operator === '+') {
+    return add(num1, num2);
+  }
+  if (operator === '-') {
+    return subtract(num1, num2);
+  }
+  if (operator === '×') {
+    return multiply(num1, num2);
+  }
+  if (operator === '÷') {
+    return divide(num1, num2);
+  }
+
+  return 'ERROR';
+};
+
+console.log(operate(1, '+', 2));
