e232785e32490690c4e9107f8a814bc90b269b74
CCWebb14 <CCWebb14@gmail.com>
2023-02-06 14:56:20 -0800

    subtract, multiply and divide functions fixed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const add = (num1, num2) => num1 + num2;
 
-const subtract = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
 
-const multiply = (num1, num2) => num1 + num2;
+const multiply = (num1, num2) => num1 * num2;
 
-const divide = (num1, num2) => num1 + num2;
+const divide = (num1, num2) => num1 / num2;
 
 const operate = (num1, operator, num2) => {
   if (operator === '+') {
